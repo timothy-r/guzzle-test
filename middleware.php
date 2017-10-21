82fcf081ab7e7508470c3c676c8963f9a6044f3b
Tim Rodger <tim.rodger@gmail.com>
2017-10-21 12:51:26 +0100

    Adds custom middlewares

---
+++ middleware.php
@@ -0,0 +1,74 @@
+<?php
+require_once ('./vendor/autoload.php');
+
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\CurlHandler;
+
+use Psr\Http\Message\RequestInterface;
+
+function add_header($header, $value)
+{
+    return function (callable $handler) use ($header, $value) {
+        return function (
+            RequestInterface $request,
+            array $options
+        ) use ($handler, $header, $value) {
+            $request = $request->withHeader($header, $value);
+            return $handler($request, $options);
+        };
+    };
+}
+
+function add_http_basic_auth($profile, $password)
+{
+    return function (callable $handler) use ($profile, $password) {
+        return function (
+            RequestInterface $request,
+            array $options
+        ) use ($handler, $profile, $password) {
+            $request = $request->withHeader("Authorization", 'Basic ' . base64_encode("$profile:$password"));
+            return $handler($request, $options);
+        };
+    };
+}
+
+function add_aws_sig_auth($profile, $password)
+{
+    return function (callable $handler) use ($profile, $password) {
+        return function (
+            RequestInterface $request,
+            array $options
+        ) use ($handler, $profile, $password) {
+            // generate signature here
+
+            return $handler($request, $options);
+        };
+    };
+}
+
+/**
+ * @param $account
+ */
+function add_auth($profile)
+{
+    // depending on $account add diff auth header(s)
+    return add_http_basic_auth($profile, '1234');
+
+}
+
+$handler = new CurlHandler();
+$stack = HandlerStack::create($handler); // Wrap w/ middleware
+//$stack->setHandler(new CurlHandler());
+
+$stack->push(add_header('X-Foo', 'bar'));
+$stack->push(add_auth('Dave', '123456x'));
+
+$client = new Client(['handler' => $stack]);
+
+$url = $argv[1];
+
+$reponse = $client->get($url);
+
+
+var_dump($reponse->getBody()->getContents());

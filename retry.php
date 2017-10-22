6f72755381dd0c05296cfd20197af9f00f3a43b1
Tim Rodger <tim.rodger@gmail.com>
2017-10-22 09:43:34 +0100

    Checks out retry middleware

--- retry.php
+++ retry.php
@@ -1,5 +1,6 @@
 <?php
 
+use GuzzleHttp\Psr7;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ConnectException;
 use GuzzleHttp\Exception\RequestException;
@@ -7,11 +8,13 @@ use GuzzleHttp\Handler\CurlHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Request as Psr7Request;
 use GuzzleHttp\Psr7\Response as Psr7Response;
+
 use Psr\Log\LoggerInterface;
+use Monolog\Logger;
 
 const MAX_RETRIES = 5;
 
-require_once __DIR__ . './vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 /**
  * @param LoggerInterface $logger
@@ -35,12 +38,15 @@ function createRetryHandler(LoggerInterface $logger)
         Psr7Response $response = null,
         RequestException $exception = null
     ) use ($logger) {
+
         if ($retries >= MAX_RETRIES) {
             return false;
         }
+
         if (!(isServerError($response) || isConnectError($exception))) {
             return false;
         }
+
         $logger->warning(sprintf(
             'Retrying %s %s %s/%s, %s',
             $request->getMethod(),
@@ -49,6 +55,7 @@ function createRetryHandler(LoggerInterface $logger)
             MAX_RETRIES,
             $response ? 'status code: ' . $response->getStatusCode() : $exception->getMessage()
         ), [$request->getHeader('Host')[0]]);
+
         return true;
     };
 }
@@ -70,3 +77,16 @@ function isConnectError(RequestException $exception = null)
 {
     return $exception instanceof ConnectException;
 }
+
+
+$url = $argv[1];
+
+$logger = new Logger('retry-logger', [], []);
+
+$client = createHttpClient($logger);
+
+$reponse = $client->get($url . "?retry=1");
+
+$str = Psr7\str($reponse);
+
+var_dump($str);
--- server/index.php
+++ server/index.php
@@ -1,5 +1,9 @@
 <?php
 
+if ($_GET['retry'] == '1') {
+    http_response_code(503);
+}
+
 var_dump(getallheaders());
 
 var_dump(file_get_contents("php://input"));
